4cff301fe4654e394f17198bbc9e656bb1162ff4
isakidev <gaspar.c.developer@gmail.com>
2024-07-17 00:23:28 -0400

    feat: add  validations and prefix api

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,8 +1,22 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+
+  app.setGlobalPrefix('api/v1')
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  )
+
+  const PORT = process.env.PORT
+
+  await app.listen(PORT)
+  console.log(`http://localhost:${PORT}/api/v1`)
 }
-bootstrap();
+bootstrap()
