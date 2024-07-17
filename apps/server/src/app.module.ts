cd39a713ec60a7dbd94dd2ca480a66bb9a29e106
isakidev <gaspar.c.developer@gmail.com>
2024-07-17 00:22:57 -0400

    feat: add config module

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,9 +1,17 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { AuthModule } from './auth/auth.module'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
