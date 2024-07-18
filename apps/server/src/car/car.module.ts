664260848bc5dc54ce2835b4b07b4f4a1202f093
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-07-18 17:13:33 -0400

    feat: new module car :tada:

---
+++ apps/server/src/car/car.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common'
+import { CarService } from './car.service'
+import { CarController } from './car.controller'
+import { PrismaModule } from 'src/prisma/prisma.module'
+
+@Module({
+  controllers: [CarController],
+  providers: [CarService],
+  imports: [PrismaModule],
+})
+export class CarModule {}
