920a9ab64abb7aceb7409a3dcc3facd6ae6fcf07
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-07-18 17:16:58 -0400

    chore: update with new error in handleError

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AuthModule } from './auth/auth.module'
+import { CarModule } from './car/car.module'
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { AuthModule } from './auth/auth.module'
       isGlobal: true,
     }),
     AuthModule,
+    CarModule,
   ],
   controllers: [AppController],
   providers: [AppService],
