7e1c52910bf4d1fd50f6ad0ecc2319309421fe5c
isakidev <gaspar.c.developer@gmail.com>
2024-07-20 23:57:14 -0400

    feat: add config jwt module

--- apps/server/src/auth/auth.module.ts
+++ apps/server/src/auth/auth.module.ts
@@ -1,11 +1,29 @@
 import { Module } from '@nestjs/common'
 
-import { AuthService } from './auth.service'
-import { AuthController } from './auth.controller'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { JwtModule } from '@nestjs/jwt'
+
 import { PrismaModule } from '../prisma/prisma.module'
 
+import { AuthService } from './auth.service'
+import { AuthController } from './auth.controller'
+
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          secret: configService.get('JWT_SECRET_KEY'),
+          signOptions: {
+            expiresIn: '1h',
+          },
+        }
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
