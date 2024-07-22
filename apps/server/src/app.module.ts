430668e3a9bb60c220a441c6bf68948d2456f791
isakidev <gaspar.c.developer@gmail.com>
2024-07-21 20:59:07 -0400

    feat: add travel module

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AuthModule } from './auth/auth.module'
 import { CarModule } from './car/car.module'
+import { TravelModule } from './travel/travel.module'
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { CarModule } from './car/car.module'
     }),
     AuthModule,
     CarModule,
+    TravelModule,
   ],
   controllers: [AppController],
   providers: [AppService],
