13c18ab591d24e37f4b67e9188c36bb40022892f
isakidev <gaspar.c.developer@gmail.com>
2024-07-22 16:57:55 -0400

    feat: implement cookie parser

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,10 +1,14 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+
+import { AppModule } from './app.module'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import * as cookieParser from 'cookie-parser'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  app.use(cookieParser())
 
   app.setGlobalPrefix('api/v1')
 
