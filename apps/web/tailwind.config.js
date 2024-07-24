d9e6dbf4a1ac9696fae173787f7156ff3daab850
isakidev <gaspar.c.developer@gmail.com>
2024-07-24 15:26:33 -0400

    feat: add static colors

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -9,17 +9,16 @@ module.exports = {
   ],
   prefix: "",
   theme: {
-    // container: {
-    //   center: true,
-    //   padding: "2rem",
-    //   screens: {
-    //     "2xl": "1400px",
-    //   },
-    // },
     extend: {
-      textColor: {
-        title: '#034363',
-        paragraph: '#6E7191'
+      colors: {
+        primary: '#034363',
+        secondary:  '#6E7191'
+      },
+      borderColor: {
+        primary: '#D3D3D3'
+      },
+      backgroundColor: {
+        primary: '#D9DBE9'
       }
     },
   },
