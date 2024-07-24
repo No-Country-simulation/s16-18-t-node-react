8a164b7219af2e3f404225208f1fa5bf0c551e25
isakidev <gaspar.c.developer@gmail.com>
2024-07-24 00:36:58 -0400

    feat: add router

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+
 import App from './App.tsx'
+
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+
+import { router } from './app/router/app.router.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <RouterProvider router={router}>
       <App />
-    </BrowserRouter>
+    </RouterProvider>
   </React.StrictMode>,
 )
