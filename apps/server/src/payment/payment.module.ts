d366df7f00ef90ad0d1e119880920913d80463a5
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-07-25 15:04:10 -0400

    fix: remove semi

--- apps/server/src/payment/payment.module.ts
+++ apps/server/src/payment/payment.module.ts
@@ -1,6 +1,6 @@
-import { Module } from '@nestjs/common';
-import { PaymentService } from './payment.service';
-import { PaymentController } from './payment.controller';
+import { Module } from '@nestjs/common'
+import { PaymentService } from './payment.service'
+import { PaymentController } from './payment.controller'
 
 @Module({
   controllers: [PaymentController],
