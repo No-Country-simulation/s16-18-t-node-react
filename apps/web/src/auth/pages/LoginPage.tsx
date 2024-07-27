010110463fdf14abdd35cf0ca39335423f61791f
isakidev <gaspar.c.developer@gmail.com>
2024-07-27 00:22:49 -0400

    style: remove space

--- apps/web/src/auth/pages/LoginPage.tsx
+++ apps/web/src/auth/pages/LoginPage.tsx
@@ -25,25 +25,25 @@ export const LoginPage = () => {
     })
 
   return (
-    <div className="text-center  pt-8">
+    <div className="text-center pt-8">
       <h2 className="pb-8">Iniciar sesión</h2>
 
       <form onSubmit={handleSubmit} className="space-y-4 pb-6">
-        <CustomInputFormik errorMessage={errors?.email} getFieldFormikProps={getFieldProps} name="email" icon={() => <EmailIcon />} placeholder="Mail" />
+        <CustomInputFormik errorMessage={errors?.email} getFieldFormikProps={getFieldProps} name="email" icon={() => <EmailIcon />} placeholder="Email" />
 
         <CustomInputFormik errorMessage={errors?.password} getFieldFormikProps={getFieldProps} name="password" icon={() => <FillLockIcon />} placeholder="Contraseña" />
 
         <div className="space-y-4">
           <a href="#" className="hover:underline text-sm text-primary">¿Olvidaste tu contraseña?</a>
 
-          <button type="submit" className='bg-tertiary py-3 text-base text-white font-medium rounded-full w-full hover:bg-tertiary/90'>Iniciar sesión</button>
+          <button type="submit" className='bg-tertiary py-3 text-base text-white font-medium rounded-full w-full hover:bg-tertiary/80'>Iniciar sesión</button>
         </div>
       </form>
 
 
       <div className="flex flex-col gap-4">
-        <a href="#" className="hover:underline text-sm text-primary">¿No tenés cuenta?</a>
-        <Link to="/auth/register" className="underline text-tertiary font-bold">Registrarme</Link>
+        <p className=" text-sm text-primary">¿No tenés cuenta?</p>
+        <Link to="/auth/register" className="underline text-tertiary font-bold hover:text-tertiary/80">Registrarme</Link>
       </div>
     </div>
   )
