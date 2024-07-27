a75efcc2fc1d653cc443541e5d6f21f9ab097be0
isakidev <gaspar.c.developer@gmail.com>
2024-07-27 02:00:06 -0400

    feat: add auth hook

---
+++ apps/web/src/auth/hooks/useAuth.tsx
@@ -0,0 +1,24 @@
+import { useBoundStore } from '@/store/bound.store'
+import { LoginRequestData } from '../interfaces/auth.interface'
+import * as authService from '../services/auth.service'
+
+export const useAuth = () => {
+  const loginStore = useBoundStore(state => state.onLogin)
+  const logoutStore = useBoundStore(state => state.onLogout)
+  const userCheckingStore = useBoundStore(state => state.onChecking)
+
+  const onLogin = async (loginData: LoginRequestData) => {
+    userCheckingStore()
+
+    return authService.loginUser(loginData)
+      .then(user => loginStore(user))
+      .catch(error => {
+        logoutStore()
+        throw error
+      })
+  }
+
+  return {
+    onLogin
+  }
+}
