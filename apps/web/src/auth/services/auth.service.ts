515fac964e3b42f5350f04e3fc81ee8b1a0942e6
isakidev <gaspar.c.developer@gmail.com>
2024-07-27 20:52:39 -0400

    feat: add origin and create renew token method

--- apps/web/src/auth/services/auth.service.ts
+++ apps/web/src/auth/services/auth.service.ts
@@ -12,3 +12,13 @@ export const loginUser = async (loginData: LoginRequestData) => {
     throw 'Internal error'
   }
 }
+
+export const renewToken = async () => {
+  try {
+    const { data } = await api.get<User>(`auth/renew-token`)
+    return data
+  } catch (error: unknown) {
+    if (isAxiosError(error)) throw error?.response?.data?.message
+    throw 'Internal error'
+  }
+}
