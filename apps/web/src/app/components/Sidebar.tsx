03ba419eb30f7dfe2926b647c89e141ea8dd9b99
isakidev <gaspar.c.developer@gmail.com>
2024-07-27 21:59:08 -0400

    feat: create sidebar component

---
+++ apps/web/src/app/components/Sidebar.tsx
@@ -0,0 +1,31 @@
+import { useAuth } from "@/auth/hooks/useAuth"
+import { LogoutIcon } from "@/common/components/icons/Logout"
+import { cn } from "@/common/utils"
+import { useBoundStore } from "@/store/bound.store"
+
+const Sidebar = () => {
+  const { onLogout } = useAuth()
+
+  const isOpenMenu = useBoundStore(state => state.isOpen)
+  const closeMenu = useBoundStore(state => state.close)
+
+  return (
+    <>
+      <div onClick={closeMenu} className={`w-full hover:cursor-pointer fixed inset-0 bg-tertiary/50 z-10 ${isOpenMenu ? 'block' : 'hidden'}`}></div >
+
+      <aside className={
+        cn('w-3/4 shadow-m p-4 transition-all duration-300 bg-gray-100 fixed top-0 bottom-0 right-0 z-20', {
+          'translate-x-[100%]': !isOpenMenu
+        })
+      }>j
+
+        <button onClick={onLogout} className="px-4 py-3 flex items-center gap-2 rounded-md text-secondary w-full hover:bg-tertiary hover:text-gray-200">
+          <LogoutIcon />
+          <span>Cerrar sesión</span>
+        </button>
+      </aside>
+    </>
+  )
+}
+
+export default Sidebar
