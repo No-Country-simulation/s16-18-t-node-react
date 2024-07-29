ff87f3d6ea57546b7f1a5f542741d6b4413e14af
isakidev <gaspar.c.developer@gmail.com>
2024-07-29 18:25:04 -0400

    feat: add  methods to get travels or travel

--- apps/web/src/travel/hooks/useTravel.tsx
+++ apps/web/src/travel/hooks/useTravel.tsx
@@ -1,12 +1,12 @@
 import { useState } from "react"
 import * as travelService from "../services/travel.service"
-import { Travel } from "../interfaces/travel.interface"
+import { SearchTravelsQueryParams, Travel } from "../interfaces/travel.interface"
 
 export const useTravel = () => {
   const [travels, setTravels] = useState<Travel[]>([])
   const [isLoading, setIsLoading] = useState<boolean>(true)
 
-  const getLastTravels = () => {
+  const onGetLastTravels = () => {
     setIsLoading(true)
 
     return travelService.getTravelsByQueryParams({ start_date: '2024-07-28' })
@@ -14,9 +14,28 @@ export const useTravel = () => {
       .finally(() => setIsLoading(false))
   }
 
+  const onGetTravelById = (travelId: string) => {
+    setIsLoading(true)
+
+    return travelService.getTravelById(travelId)
+      .then(data => setTravels([data]))
+      .finally(() => setIsLoading(false))
+  }
+
+  const onGetTravelsByQueryParams = async (filters: SearchTravelsQueryParams) => {
+    setIsLoading(true)
+
+    return travelService.getTravelsByQueryParams(filters)
+      .then(data => setTravels(data))
+      .finally(() => setIsLoading(false))
+  }
+
   return {
     isLoading,
-    getLastTravels,
-    travels
+    travels,
+
+    onGetLastTravels,
+    onGetTravelsByQueryParams,
+    onGetTravelById
   }
 }
