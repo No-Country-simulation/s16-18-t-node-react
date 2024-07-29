431f9aed4e1087d54aa2466850ddc37bb3effe8d
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-07-29 18:18:42 -0400

    feat: export service

--- apps/server/src/ratings/ratings.module.ts
+++ apps/server/src/ratings/ratings.module.ts
@@ -7,5 +7,6 @@ import { PrismaModule } from 'src/prisma/prisma.module'
   imports: [PrismaModule],
   controllers: [RatingsController],
   providers: [RatingsService],
+  exports: [RatingsService],
 })
 export class RatingsModule {}
