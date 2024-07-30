5ebbf0c85bc70b0ac8fbf1e9392becbabec02dc3
isakidev <gaspar.c.developer@gmail.com>
2024-07-30 18:52:21 -0400

    chore: remove unused function

--- apps/web/src/travel/hooks/useTravel.tsx
+++ apps/web/src/travel/hooks/useTravel.tsx
@@ -6,14 +6,6 @@ export const useTravel = () => {
   const [travels, setTravels] = useState<Travel[]>([])
   const [isLoading, setIsLoading] = useState<boolean>(true)
 
-  const onGetLastTravels = () => {
-    setIsLoading(true)
-
-    return travelService.getTravelsByQueryParams({ start_date: '2024-07-28' })
-      .then(data => setTravels(data))
-      .finally(() => setIsLoading(false))
-  }
-
   const onGetTravelById = (travelId: string) => {
     setIsLoading(true)
 
@@ -22,7 +14,7 @@ export const useTravel = () => {
       .finally(() => setIsLoading(false))
   }
 
-  const onGetTravelsByQueryParams = async (filters: SearchTravelsQueryParams) => {
+  const onGetTravelsByQueryParams = (filters: SearchTravelsQueryParams) => {
     setIsLoading(true)
 
     return travelService.getTravelsByQueryParams(filters)
@@ -34,7 +26,6 @@ export const useTravel = () => {
     isLoading,
     travels,
 
-    onGetLastTravels,
     onGetTravelsByQueryParams,
     onGetTravelById
   }
