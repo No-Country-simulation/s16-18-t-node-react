52208499300dfe124a67ca5831563cfa83a8e50f
isakidev <gaspar.c.developer@gmail.com>
2024-07-31 15:48:21 -0400

    feat: update changes

--- apps/web/src/payment/router/payment.router.tsx
+++ apps/web/src/payment/router/payment.router.tsx
@@ -1,14 +1,9 @@
 import { Navigate, type RouteObject } from "react-router-dom";
-<<<<<<< HEAD
 
-import { DetailPaymentMethodPage } from "../pages/DetailPaymentMethodPage.tsx";
 import { SuccessPage } from "../pages/SuccessPage.tsx";
 import { DetailPaymentSummaryPage } from "../pages/DetailPaymentSummaryPage.tsx";
-=======
-import { DetailPaymentPage } from '../pages/DetailPaymentPage.tsx'
-import { SuccessPage } from "../pages/SuccessPage.tsx";
 import { DetailPaymentMethodMPPage } from "../pages/DetailPaymentMethodMPPage.tsx";
->>>>>>> develop
+
 
 export const paymentRouter: RouteObject[] = [
   {
